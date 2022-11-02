8f71ce3734e91af80b4525af0201c0aac2d4dc35
Victoria Maggay <toria0305@gmail.com>
2022-11-02 00:11:08 -0400

    commit tree 336ee25072d87dc22ce2f3fcb2955eedee4c5f36

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield",];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    cats.splice(-1);
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift(1);
+    return cats;
+}
+function appendCat(name) {
+    let appendCat;
+    appendCat = [...cats, name];
+    return appendCat;
+}
+function prependCat(name) {
+    let prependCat;
+    prependCat = [name, ...cats];
+    return prependCat;
+}
+ function removeLastCat() {
+    return cats.slice(0,cats.length -1);
+ }
+function removeFirstCat() {
+    return cats.slice(1);
+}
